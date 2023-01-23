311ed32e05a50fc0c68aa2707950d44ac22bd6b5
Jonathan Face <jonathanjface@gmail.com>
2023-01-22 23:27:09 -0600

    basic formatting

--- static/rd-ui/src/sections/document.js
+++ static/rd-ui/src/sections/document.js
@@ -1,6 +1,5 @@
 import React, {useRef} from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState, ContentState, CompositeDecorator, SelectionState} from 'draft-js';
+import {Editor, EditorState, ContentState, CompositeDecorator, RichUtils, getDefaultKeyBinding, Modifier, SelectionState} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import '../css/document.css';
 import { Menu, Item, Submenu, useContextMenu } from 'react-contexify';
@@ -93,8 +92,41 @@ const createDecorators = () => {
       }
     });
   });
+  decorators.push({
+    strategy: findTabs,
+    component: TabSpan
+  });
   return new CompositeDecorator(decorators);
 }
+
+const findTabs = (contentBlock, callback, contentState) => {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity();
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === 'TAB'
+    );
+  },
+  callback);
+}
+
+const TabSpan = (props) => {
+  return (
+    <span className="tabEntity">{props.children}</span>
+  );
+};
+
+const tabLength = 5;
+const generateTabCharacter = () => {
+  let tab = '';
+  for (let i=0; i < tabLength; i++) {
+    tab += ' ';
+  }
+  return tab;
+}
+
+
+
 
 const getSelectedText = (editorState) => {
   const selection = editorState.getSelection();
@@ -121,7 +153,26 @@ function Document() {
     domEditor.current.focus();
   }
 
-  const forceStateUpdate = () => {
+  const keyBindings = (event) => {
+    // tab pressed
+    if (event.keyCode == 9) {
+      event.preventDefault();
+      setEditorState(insertTab(editorState));
+    }
+    return getDefaultKeyBinding(event);
+  }
+
+  const setFocusAndRestoreCursor = (editorState) => {
+    const selection = editorState.getSelection();
+      const newSelection = selection.merge({
+        anchorOffset: selection.getIsBackward() ? selection.getAnchorOffset() : selection.getFocusOffset(),
+        focusOffset: selection.getIsBackward() ? selection.getAnchorOffset() : selection.getFocusOffset()
+      })
+      setFocus();
+      return EditorState.forceSelection(editorState, newSelection);
+  }
+
+  const forceStateUpdate = (editorState) => {
     return EditorState.set(editorState, {decorator: createDecorators()});
   }
 
@@ -136,14 +187,9 @@ function Document() {
       // check if !contains
       console.log("creating new", id)
       associations.push({type:id, name:text, details:{aliases:""}});
-      const selection = editorState.getSelection();
-      const newSelection = selection.merge({
-        anchorOffset:selection.getFocusOffset()
-      })
-      setFocus();
-      const newEditorState = forceStateUpdate();
-      const withSelection = EditorState.forceSelection(newEditorState, newSelection);
-      setEditorState(withSelection);
+      const withSelection = setFocusAndRestoreCursor(editorState)
+      const newEditorState = forceStateUpdate(withSelection);
+      setEditorState(newEditorState);
     }
   };
 
@@ -160,10 +206,51 @@ function Document() {
     }
   }
 
+  const handleStyleClick = (event, style) => {
+    event.preventDefault();
+    setEditorState(RichUtils.toggleInlineStyle(editorState, style));
+  }
+
+  const handleKeyCommand = (command) => {
+    setEditorState(RichUtils.handleKeyCommand(editorState, command));
+  }
+
+  const insertTab = (editorState) => {
+    console.log('inserting tab');
+    const selection = editorState.getSelection();
+    const currentContent = editorState.getCurrentContent();
+    const contentStateWithEntity = currentContent.createEntity('TAB', 'IMMUTABLE');
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    const textWithEntity = Modifier.insertText(currentContent, selection, generateTabCharacter(), null, entityKey);
+    const newState = EditorState.push(editorState, textWithEntity, 'apply-entity');
+    return EditorState.forceSelection(newState, textWithEntity.getSelectionAfter());
+  }
+
+  const updateEditorState = (newEditorState) => {
+    const blockTree = editorState.getBlockTree(newEditorState.getSelection().getFocusKey());
+    if (!blockTree) {
+      // new paragraph added
+      const thisBlock = newEditorState.getCurrentContent().getBlockForKey(newEditorState.getSelection().getFocusKey());
+      const firstChar = thisBlock.getCharacterList().get(0);
+      if ((!firstChar || firstChar && firstChar.entity == null) ||
+          (!firstChar || newEditorState.getCurrentContent().getEntity(firstChar.entity).getType() != 'TAB')) {
+          newEditorState = insertTab(newEditorState);
+        }
+
+    }
+    setEditorState(newEditorState);
+  }
+
   return (
     <div>
+      <nav className="rich-controls">
+        <button onMouseDown={(e) => {handleStyleClick(e,'BOLD')}}><b>B</b></button>
+        <button onMouseDown={(e) => {handleStyleClick(e,'ITALIC')}}><i>I</i></button>
+        <button onMouseDown={(e) => {handleStyleClick(e,'UNDERLINE')}}><u>U</u></button>
+        <button onMouseDown={(e) => {handleStyleClick(e,'STRIKETHROUGH')}}><s>S</s></button>
+      </nav>
       <section onContextMenu={handleContextMenu}>
-        <Editor editorState={editorState} onChange={setEditorState} ref={domEditor} />
+        <Editor editorState={editorState} onChange={updateEditorState} handleKeyCommand={handleKeyCommand} keyBindingFn={keyBindings} ref={domEditor} />
       </section>
       <Menu id="custom_context">
         <Submenu label="Create Association">

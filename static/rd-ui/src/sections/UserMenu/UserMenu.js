cc18284c87f5f5959d3b1680bafa017587270cd2
Jonathan Face <jonathanjface@gmail.com>
2023-10-21 22:46:34 -0500

    corrected some graphical glitches

--- static/rd-ui/src/sections/UserMenu/UserMenu.js
+++ static/rd-ui/src/sections/UserMenu/UserMenu.js
@@ -7,7 +7,7 @@ import { flipLoggedInState } from '../../stores/loggedInSlice';
 import { setSelectedSeries } from '../../stores/selectedSeriesSlice';
 import { setSelectedStoryTitle } from '../../stores/selectedStorySlice';
 
-const UserMenu = () => {
+const UserMenu = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const isLoggedIn = useSelector((state) => state.isLoggedIn.value);
   const dispatch = useDispatch();
@@ -35,27 +35,30 @@ const UserMenu = () => {
     });
   };
 
+  console.log("prop", props.isParentLoading);
+
+  const displayComponent =
+    !props.isParentLoading
+      ? isLoggedIn
+        ? <span className="menu-container" onClick={()=>setIsOpen(!isOpen)} onMouseEnter={()=>setIsOpen(true)} onMouseLeave={()=>setIsOpen(false)}>
+            <span className="icon">
+              <IconButton size="small" sx={{zIndex: 99}} aria-label="user menu" >
+                <Person4Icon fontSize="small" className={'menu-icon'}/>
+              </IconButton>
+            </span>
+            {isOpen && (
+              <ul>
+                <li>Settings</li>
+                <li onClick={signout}>Signout</li>
+              </ul>
+            )}
+          </span>
+        : <a onClick={signin}>SignIn</a>
+      : <div/>
+
   return (
     <span className="user-menu">
-      {isLoggedIn && (
-        <span className="menu-container" onClick={()=>setIsOpen(!isOpen)} onMouseEnter={()=>setIsOpen(true)} onMouseLeave={()=>setIsOpen(false)}>
-          <span className="icon">
-            <IconButton size="small" sx={{zIndex: 99}} aria-label="user menu" >
-              <Person4Icon fontSize="small" className={'menu-icon'}/>
-            </IconButton>
-          </span>
-          {isOpen && (
-            <ul>
-              <li>Settings</li>
-              <li onClick={signout}>Signout</li>
-            </ul>
-          )}
-
-        </span>
-      )}
-      {!isLoggedIn && (
-        <a onClick={signin}>SignIn</a>
-      )}
+      {displayComponent}
     </span>
   );
 };

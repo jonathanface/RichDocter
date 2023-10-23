b23a811e28783c323ad626a8745c447590bfa1ad
Jonathan Face <jonathanjface@gmail.com>
2023-10-22 23:51:14 -0500

    non-youtube video

--- static/rd-ui/src/sections/storyAndSeriesListing/StoryAndSeriesListing.js
+++ static/rd-ui/src/sections/storyAndSeriesListing/StoryAndSeriesListing.js
@@ -39,7 +39,6 @@ const StoryAndSeriesListing = () => {
         });
         setSeriesGroups(seriesStoriesFromDB);
         setSeriesLoaded(true);
-        setTotalWorks(totalWorks + seriesStoriesFromDB.size);
       });
     };
 

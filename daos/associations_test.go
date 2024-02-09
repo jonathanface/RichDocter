9f60fdb4e299c87e00ccd68b2dc83b4b2a7e4573
Jonathan Face <jonathanjface@gmail.com>
2024-02-08 22:40:53 -0600

    mocking dynamodb = pain in my ass

--- daos/associations_test.go
+++ daos/associations_test.go
@@ -24,7 +24,6 @@ func TestWriteAssociations(t *testing.T) {
 		},
 	}
 
-	// Wrap it in an OperationError
 	opErr := &smithy.OperationError{
 		ServiceID:     "DynamoDB",
 		OperationName: "TransactWriteItems",
@@ -111,7 +110,6 @@ func TestWriteAssociations(t *testing.T) {
 		},
 	}
 
-	// Iterate through test cases
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			dao := daos.NewMock()
@@ -119,6 +117,117 @@ func TestWriteAssociations(t *testing.T) {
 				tc.setupMock(dao.DynamoClient.(*daos.MockDynamoDBClient))
 			}
 			results := dao.WriteAssociations(tc.email, tc.storyOrSeriesID, tc.associations)
+			if tc.expectError {
+				assert.Error(t, results)
+			} else {
+				assert.NoError(t, results)
+			}
+		})
+	}
+}
+
+func TestUpdateAssociationPortraitEntryInDB(t *testing.T) {
+	testCases := []struct {
+		name                                       string // name of the test case
+		email, storyOrSeriesID, associationID, url string // inputs
+		setupMock                                  func(*daos.MockDynamoDBClient)
+		expectError                                bool
+	}{
+		{
+			"happy path",
+			"test@test.com",
+			"12345",
+			"seriesID",
+			"http://www.google.com",
+			func(cl *daos.MockDynamoDBClient) {
+				cl.On("UpdateItem", mock.Anything, mock.AnythingOfType("*dynamodb.UpdateItemInput"), mock.Anything).Return(&dynamodb.UpdateItemOutput{}, nil).Once()
+			},
+			false,
+		},
+		{
+			"db error",
+			"test@test.com",
+			"12345",
+			"seriesID",
+			"http://www.google.com",
+			func(cl *daos.MockDynamoDBClient) {
+				cl.On("UpdateItem", mock.Anything, mock.AnythingOfType("*dynamodb.UpdateItemInput"), mock.Anything).Return(&dynamodb.UpdateItemOutput{}, fmt.Errorf("some error")).Once()
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			dao := daos.NewMock()
+			if tc.setupMock != nil {
+				tc.setupMock(dao.DynamoClient.(*daos.MockDynamoDBClient))
+			}
+			results := dao.UpdateAssociationPortraitEntryInDB(tc.email, tc.storyOrSeriesID, tc.associationID, tc.url)
+			if tc.expectError {
+				assert.Error(t, results)
+			} else {
+				assert.NoError(t, results)
+			}
+		})
+	}
+}
+
+func TestDeleteAssociations(t *testing.T) {
+	// txnCanceledErr := &types.TransactionCanceledException{
+	// 	CancellationReasons: []types.CancellationReason{
+	// 		{
+	// 			Code:    aws.String("ConditionalCheckFailed"),
+	// 			Message: aws.String("Example failure message"),
+	// 		},
+	// 	},
+	// }
+
+	// opErr := &smithy.OperationError{
+	// 	ServiceID:     "DynamoDB",
+	// 	OperationName: "TransactWriteItems",
+	// 	Err:           txnCanceledErr,
+	// }
+
+	testCases := []struct {
+		name           string // name of the test case
+		email, storyID string // inputs
+		setupMock      func(*daos.MockDynamoDBClient)
+		associations   []*models.Association
+		expectError    bool
+	}{
+		{
+			"happy path",
+			"test@test.com",
+			"12345",
+			func(cl *daos.MockDynamoDBClient) {
+				//d.DynamoClient.Scan(context.TODO(), scanInput)
+				cl.On("Scan", mock.Anything, mock.AnythingOfType("*dynamodb.ScanInput"), mock.Anything).Return(&dynamodb.ScanOutput{Items: []map[string]types.AttributeValue{
+					{
+						"user": &types.AttributeValueMemberS{Value: "1"},
+					},
+				}}, nil).Once()
+				cl.On("Scan", mock.Anything, mock.AnythingOfType("*dynamodb.ScanInput"), mock.Anything).Return(&dynamodb.ScanOutput{Items: []map[string]types.AttributeValue{
+					{
+						"story": &types.AttributeValueMemberS{Value: "1"},
+					},
+				}}, nil).Once()
+				cl.On("Scan", mock.Anything, mock.AnythingOfType("*dynamodb.ScanInput"), mock.Anything).Return(&dynamodb.ScanOutput{}, nil).Once()
+				cl.On("TransactWriteItems", mock.Anything, mock.AnythingOfType("*dynamodb.TransactWriteItemsInput"), mock.Anything).Return(&dynamodb.TransactWriteItemsOutput{}, nil).Twice()
+			},
+			[]*models.Association{{
+				ID: "1234",
+			}},
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			dao := daos.NewMock()
+			if tc.setupMock != nil {
+				tc.setupMock(dao.DynamoClient.(*daos.MockDynamoDBClient))
+			}
+			results := dao.DeleteAssociations(tc.email, tc.storyID, tc.associations)
 			if tc.expectError {
 				assert.Error(t, results)
 			} else {

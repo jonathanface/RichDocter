332365635cf1bc039dece272b66be66565b31e62
Jonathan Face <jonathan.face@protonmail.com>
2025-02-21 09:21:36 -0600

    linted

--- static/rd-ui/src/sections/CreateEditStorySlideshow/index.tsx
+++ static/rd-ui/src/sections/CreateEditStorySlideshow/index.tsx
@@ -15,7 +15,6 @@ import { useToaster } from "../../hooks/useToaster";
 import { AlertToastType } from "../../types/AlertToasts";
 import { useNavigate, useParams } from "react-router-dom";
 import CloseIcon from '@mui/icons-material/Close';
-import { useFetchUserData } from "../../hooks/useFetchUserData";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 
 const steps = ['Title', 'Image', 'Description', 'Series'];

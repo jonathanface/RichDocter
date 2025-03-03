408fb59f70f0511bed31c3042dbc9fa9296b593f
Jonathan Face <jonathan.face@protonmail.com>
2025-03-03 13:40:46 -0600

    fixed not-updating story edits

--- static/rd-ui/src/contexts/worksList.tsx
+++ static/rd-ui/src/contexts/worksList.tsx
@@ -2,11 +2,11 @@ import { createContext } from "react";
 import { Series } from "../types/Series";
 import { Story } from "../types/Story";
 
-type WorksList = {
+type WorksListContextType = {
     seriesList: Series[] | null;
     storiesList: Story[] | null;
     setSeriesList: (list: Series[] | null) => void;
     setStoriesList: (list: Story[] | null) => void;
 };
 
-export const WorksListContext = createContext<WorksList | undefined>(undefined);
+export const WorksListContext = createContext<WorksListContextType | undefined>(undefined);

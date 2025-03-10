650615b80534547e1e7126f8c8d2d06e018cb37b
Jonathan Face <jonathan.face@protonmail.com>
2025-03-09 21:37:08 -0500

    splash page fixes

--- static/rd-ui/src/sections/SplashPage/index.tsx
+++ static/rd-ui/src/sections/SplashPage/index.tsx
@@ -14,14 +14,14 @@ export const SplashPage = () => {
     <div className={styles.splash}>
       <div className={styles.introText}>
         <h4>Docter is the writing platform that manages all your story elements – so you can focus on crafting an unforgettable tale.</h4>
-        Built with the serious writer in mind, we combine a streamlined text editor with built-in organizational tools for characters, places and events.  You can draft chapters, annotate references on the fly, and keep all story elements neatly connected in one place. It’s a simple, flexible environment that helps you stay focused on writing while effortlessly managing the details of your world.
+        Developed with the serious writer in mind, we combine a streamlined text editor with built-in organizational tools for characters, places and events.  You can draft chapters, annotate references on the fly, and keep all story elements neatly connected in one place. It’s a simple, flexible environment that helps you stay focused on writing while effortlessly managing the fine details of your world.
         <div className={styles.loginBtn}>
           <a onClick={showLoginPanel}>Try It Out</a>
         </div>
       </div>
       <div className={styles.blurb}>
         <div className={styles.demoHeader}>
-          <h4>Excerpt: <a href="https://www.amazon.com/Remnants-Dead-Loss-Jonathan-Face-ebook/dp/B071V6BV9J" target="_blank"><i>The Remnants: Dead Loss</i></a> by Jonathan Face</h4>
+          <h4>Editable Excerpt: <a href="https://www.amazon.com/Remnants-Dead-Loss-Jonathan-Face-ebook/dp/B071V6BV9J" target="_blank"><i>The Remnants: Dead Loss</i></a> by Jonathan Face</h4>
         </div>
         <div className={styles.demo}>
 
@@ -31,7 +31,7 @@ export const SplashPage = () => {
       <div className={styles.blurb}>
         <span className={styles.column + " " + styles.leftText}>
           <h2>Introducing the Next-Generation Storycrafting Platform...</h2>
-          <p><h4>...where every keystroke becomes an immersive journey into your own world.</h4></p>
+          <div><h4>...where every keystroke becomes an immersive journey into your own world.</h4></div>
           <p>Seamlessly draft chapters, forge dynamic annotations, and hyperlink your characters, places, and events with a single click. Whether you're weaving complex epics or penning a heartfelt tale, this writing tool wraps you in an intuitive, distraction-free editor that feels like a natural extension of your creative mind. From powerful organizational features to simple-yet-sophisticated text management, everything is designed to help you focus on what matters most—telling a story that captivates, resonates, and inspires.</p>
           <p>Embrace a writing environment where every character truly comes alive on the page—welcome to your new creative home!</p>
         </span>

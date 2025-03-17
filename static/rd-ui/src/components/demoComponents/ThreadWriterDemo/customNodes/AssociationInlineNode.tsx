b129b7d7391841e383b242b64b6f36b0b12bd609
Jonathan Face <jonathan.face@protonmail.com>
2025-03-16 23:28:18 -0500

    simplify

--- static/rd-ui/src/components/demoComponents/ThreadWriterDemo/customNodes/AssociationInlineNode.tsx
+++ static/rd-ui/src/components/demoComponents/ThreadWriterDemo/customNodes/AssociationInlineNode.tsx
@@ -132,7 +132,7 @@ export class AssociationInlineNode extends TextNode {
 
     showTooltip = (event: MouseEvent) => {
         if (tooltipElement !== null) return;
-        document.querySelectorAll('#association-tooltip').forEach(el => el.remove());
+        this.hideHovers();
         tooltipElement = document.createElement('div');
         tooltipElement.id = 'association-tooltip';
         const tooltip = tooltipElement;
